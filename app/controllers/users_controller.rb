3c87c92a64fb3d899cddea12eb22d29e1dbf8797
khaled83 <khaled@supahands.com>
2018-05-22 23:32:24 +0800

    Centralized authenticate_request! into ApplicationController and added more http methods to CORS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,8 @@
-class UsersController < ApplicationController
+class UsersController < JsonApiController
   before_action :authenticate_request!
 
   def index
     render json: [{ name: 'Qui' }, { name: 'Quo' }, { name: 'Qua' }]
-  end
-
-  private
-
-  def authenticate_request!
-      # byebug
-    if valid_token?
-      @current_user = User.find(auth_token[:user_id].second)
-    else
-      render json: {}, status: :unauthorized
-    end
-  rescue JWT::VerificationError, JWT::DecodeError, Omniauth::ResponseError, Omniauth::PermissionError
-    render json: {}, status: :unauthorized
-  end
-
-  private
-
-  def valid_token?
-    request.headers['Authorization'].present? && auth_token.present?
-  end
-
-  private
-
-  def valid_token?
-    request.headers['Authorization'].present? && auth_token.present?
-  end
-
-  def auth_token
-    @auth_token ||= JsonWebTokenService.decode(request.headers['Authorization'].split(' ').last)
   end
 
   # def auth_token
