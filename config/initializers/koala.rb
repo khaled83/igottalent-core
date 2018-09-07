e35c831fff9966bbe099604c065447503a0ec08a
khaled83 <khaled@supahands.com>
2018-09-07 11:04:21 +0800

    Using secret or ENV variables for fb params

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,8 +1,8 @@
 Koala.configure do |config|
-  config.access_token = Rails.application.secrets['facebook_app_access_token']
+  config.access_token = Rails.application.secrets['facebook_app_access_token'] || ENV['FACEBOOK_APP_ACCESS_TOKEN']
   # config.app_access_token = MY_APP_ACCESS_TOKEN
-  config.app_id = Rails.application.secrets['facebook_app_id']
-  config.app_secret = Rails.application.secrets['facebook_app_secret']
+  config.app_id = Rails.application.secrets['facebook_app_id'] || ENV['FACEBOOK_APP_ID']
+  config.app_secret = Rails.application.secrets['facebook_app_secret'] || ENV['FACEBOOK_APP_SECRET']
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
